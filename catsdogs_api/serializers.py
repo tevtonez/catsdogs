7a665ff246ec83c9a4e13ae1bf20c29a45978706
tevtonez <tevtonez@gmail.com>
2017-11-02 18:54:10 +0100

    add static

--- catsdogs_api/serializers.py
+++ catsdogs_api/serializers.py
@@ -8,11 +8,10 @@ class CatSerializer(serializers.Serializer):
     class Meta:
         model = models.Cat
         fields = ('id', 'animal_owner', 'cat_name', 'cat_bd')
-        extra_kwargs = {'user_profile': {'read_only': True}}
 
 class DogSerializer(serializers.Serializer):
     """Serializes a cat"""
     class Meta:
         model = models.Dog
         fields = ('id', 'animal_owner', 'dog_name', 'dog_bd')
-        #extra_kwargs = {'user_profile': {'read_only': True}}
+
