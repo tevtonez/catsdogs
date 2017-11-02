a4199ab7c1a0bfa126e69ae9611ab9f1f9757c43
tevtonez <tevtonez@gmail.com>
2017-11-02 18:56:49 +0100

    add ModelSerializer to cats and dogs

--- catsdogs_api/serializers.py
+++ catsdogs_api/serializers.py
@@ -3,13 +3,15 @@ from rest_framework import serializers
 from main import models
 
 
-class CatSerializer(serializers.Serializer):
+class CatSerializer(serializers.ModelSerializer):
     """Serializes a cat"""
     class Meta:
         model = models.Cat
         fields = ('id', 'animal_owner', 'cat_name', 'cat_bd')
 
-class DogSerializer(serializers.Serializer):
+
+
+class DogSerializer(serializers.ModelSerializer):
     """Serializes a cat"""
     class Meta:
         model = models.Dog
